40b8d7ebbd9f978b1c1f79c34a099501488b8e3e
Your Name <jigneshmakvana077@gmail.com>
2024-12-29 11:13:11 +0530

    last changes maked ( i think backend is complete )

--- src/Controller/like.controller.js
+++ src/Controller/like.controller.js
@@ -71,12 +71,65 @@ const ToggleCommentLike = async (req, res) => {
         }
 
     } catch (error) {
-        console.log(error)
+        return res.status(500).json({ message: "Somthing wrong try agian" })
+    }
+}
+
+const GetVideoLikeCount = async (req,res) => {
+    try {
+        let Videoid = req.params.videoid;
+        Videoid = Videoid.replace(/^:/, "")
+
+        if (!mongoose.Types.ObjectId.isValid(Videoid)) {
+            return res.status(400).json({ message: "Invalid video ID" });
+        }
+
+        const LikeCount = await Like.countDocuments({ Videos : Videoid })
+
+        if(!LikeCount){
+            return res.status(400).json({ message: "like Not Found" });
+        }
+
+        return res.status(200).json({
+            message: "Video like count fetched successfully",
+            LikeCount
+        });
+
+    } catch (error) {
+        return res.status(500).json({ message: "Somthing wrong try agian" })
+    }
+}
+
+const GetCommentLikeCount = async (req,res) => {
+    try {
+        let CommentID = req.params.commentid
+        CommentID = CommentID.replace(/^:/, "")
+
+        if (!mongoose.Types.ObjectId.isValid(CommentID)) {
+            return res.status(400).json({ message: "Invalid video ID" });
+        }
+
+        const CommentLikeCount = await Like.countDocuments({
+            Commnets : CommentID
+        })
+
+        if(!CommentLikeCount){
+            return res.status(400).json({ message: "Comment Not Found" });
+        }
+
+        return res.status(200).json({
+            message: "Comment like count fetched successfully",
+            CommentLikeCount
+        });
+
+    } catch (error) {
         return res.status(500).json({ message: "Somthing wrong try agian" })
     }
 }
 
 export {
     ToggleVideoLike,
-    ToggleCommentLike
+    ToggleCommentLike,
+    GetVideoLikeCount,
+    GetCommentLikeCount
 }
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import VerifyJWT from '../midelware/JWT.midelware.js'
-import { ToggleCommentLike, ToggleVideoLike } from '../Controller/like.controller.js'
+import { GetCommentLikeCount, GetVideoLikeCount, ToggleCommentLike, ToggleVideoLike } from '../Controller/like.controller.js'
 
 const LikeRouter = express.Router()
 
 LikeRouter.route('/addliketovideo/:videoid').post(VerifyJWT, ToggleVideoLike)
 LikeRouter.route('/addliketocomment/:commentid').post(VerifyJWT, ToggleCommentLike)
+LikeRouter.route('/getvideolikecount/:videoid').get(GetVideoLikeCount)
+LikeRouter.route('/getcommentlikecount/:commentid').get(GetCommentLikeCount)
 
 export default LikeRouter
