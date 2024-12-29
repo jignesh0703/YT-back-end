86c0d2ee5360fc760189ae808321471dc1ade5c8
Your Name <jigneshmakvana077@gmail.com>
2024-12-29 16:30:18 +0530

    last one

--- src/server.js
+++ src/server.js
@@ -1,17 +1,23 @@
 import dotenv from 'dotenv'
 import ConnectDB from './DB/ConnectDB.js'
 import app from './app.js'
+import cors from 'cors'
 
 dotenv.config({
-    path : './.env'
+    path: './.env'
 })
 
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    credentials: true
+}))
+
 ConnectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 3000, ()=> {
-        console.log(`Server is running on http://localhost:${process.env.PORT}`)
+    .then(() => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running on http://localhost:${process.env.PORT}`)
+        })
     })
-})
-.catch((err)=>{
-    console.log("MONGO db connection failed !!! ", err)
-})
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err)
+    })
