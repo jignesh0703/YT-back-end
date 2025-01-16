a76a503fc2aa4f345dd0257cb364b464a8b74af8
Your Name <jigneshmakvana077@gmail.com>
2025-01-16 19:22:39 +0530

    neww

--- src/midelware/multer.midelwar.js
+++ src/midelware/multer.midelwar.js
@@ -1,13 +1,22 @@
 import multer from 'multer'
+import fs from 'fs';
 
 const storage = multer.diskStorage({
-    destination : (req,file,cb) => {
-        cb(null,'./public/upload')
+    destination: (req, file, cb) => {
+        const uploadDir = './public/upload';
+        
+        // Ensure the upload directory exists
+        if (!fs.existsSync(uploadDir)) {
+            console.log('Creating upload directory...');
+            fs.mkdirSync(uploadDir, { recursive: true });
+        }
+        
+        cb(null, uploadDir);
     },
-    filename: function (req,file,cb) {
-        cb(null,file.originalname)
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
     }
-})
+});
 
 export const upload = multer({
     storage
