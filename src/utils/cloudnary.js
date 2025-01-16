dac66e5876eecf62bd0cbbafe29610ec3d93a299
Your Name <jigneshmakvana077@gmail.com>
2025-01-16 19:37:41 +0530

    add

--- src/utils/cloudnary.js
+++ src/utils/cloudnary.js
@@ -18,38 +18,27 @@ const UploadOnCloudinary = async (filePath) => {
             return null;
         }
 
-        // Check if the file exists locally
-        if (!fs.existsSync(filePath)) {
-            console.log(`File does not exist at path: ${filePath}`);
-            return null;
-        }
-
         // Normalize the file path (in case there are issues with backslashes)
         const normalizedFilePath = path.resolve(filePath);
-        console.log(`Uploading file to Cloudinary from: ${normalizedFilePath}`);
+        console.log(normalizedFilePath)
 
         const response = await cloudinary.uploader.upload(normalizedFilePath, {
             resource_type: 'auto',
         });
-
-        console.log("File uploaded successfully to Cloudinary:", response);
+        console.log(response)
 
         // Delete the file from the local system after uploading
         fs.unlinkSync(normalizedFilePath);
-        console.log("Local file deleted after upload");
 
         // Return the Cloudinary response with the uploaded file info
         return response;
 
     } catch (error) {
-        console.log("Error during upload to Cloudinary:", error);
-
-        // Ensure that the file is deleted from the local system in case of an error
+        // Make sure to remove the file in case of an error
         if (fs.existsSync(filePath)) {
             fs.unlinkSync(filePath);
-            console.log("Local file deleted after upload failure");
         }
-
+        console.log('somthing wrong')
         return null;
     }
 };
