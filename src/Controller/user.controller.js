e2663560982c56258c6ff6a3f775eb622d3d5e50
Your Name <jigneshmakvana077@gmail.com>
2025-01-17 19:49:56 +0530

    spelling mistake

--- src/Controller/user.controller.js
+++ src/Controller/user.controller.js
@@ -115,7 +115,7 @@ const login = async (req, res) => {
         const options = {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production", // Ensure secure cookies only in production
-            sameSite: 'strict'
+            sameSite: 'Strict'
         }
 
         res.cookie('accesstoken', accesstoken, options)
