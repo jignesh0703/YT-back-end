5eb5224f3d7511774af4523fe6d24b4b9e506a5c
Your Name <jigneshmakvana077@gmail.com>
2025-01-23 19:49:31 +0530

    new cahnge

--- src/midelware/multer.midelwar.js
+++ src/midelware/multer.midelwar.js
@@ -3,5 +3,8 @@ import multer from 'multer'
 const storage = multer.memoryStorage()
 
 export const upload = multer({
-    storage
+    storage,
+    limits : {
+        fileSize: 200 * 1024 * 1024,
+    }
 })
